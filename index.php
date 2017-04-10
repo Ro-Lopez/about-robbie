cc2de070a7aa1f3d449956f818f75055a80ae436
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-04-10 14:00:25 -0600

    Editing Robbie's about me page. Mostly styling, and a little humor.

--- index.php
+++ index.php
@@ -1,17 +1,33 @@
 <!doctype html>
 <HTML lang="en">
 	<head>
+		<style>
+			footer { color:red}
+			footer {  background: #000;}
+			footer {text-align: center}
+			h1 {  background-color: red;}
+			body	{width: 600px; margin: 20px auto; padding: 20px;  border: 3px solid black;}
+			p {text-align: center}
+			h1 {text-align: center}
+		</style>
 		<title>
-			<h1>Robbie - About Me</h1>
+			Robbie - About Me
 		</title>
 
 	</head>
 	<BODY>
-		<p>Hello my name is Robbie</p>
-		<p>I love staying active, especially activities that I can bring my dogs along.</p>
-		<p>I also like working out and taking self defense classes</p>
-		<IMG SRC="images\aboutpic.jpg" alt="rottweiler"/>
-
+		<h1>Hello, my name is Robbie!</h1>
+		<p>I love staying active, especially activities where I can bring my dogs along.<br>
+		I also like working out and taking self defense classes.<br><br>
+		Basically, if you're human, I might hit you.
+			<br>If you're a dog, I probably love you...<br>
+		...especially if you're a rottweiler!</p>
+		<p><IMG SRC="images/aboutpic.jpg" alt="rottweiler" height="480" width="268" align="center">
+		</p><br>
+		<p>These are my dogs. I prefer to showcase their faces rather than show my own.
+			<br>Do you like dogs too? Check out these <a href="http://www.akc.org/dog-breeds/rottweiler/">rottweilers!</a>
+		</p>
+		<footer>Must Love Dogs</footer>
 
 	</body>
 </HTML>
