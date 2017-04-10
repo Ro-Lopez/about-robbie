aff298598564c4a2f6fe1375acf6e732513b49f5
Robbie Lopez <rolopez.email@gmail.com>
2017-04-10 11:44:08 -0600

    rough draft about me

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<HTML lang="en">
+	<head>
+		<title>
+			<h1>Robbie - About Me</h1>
+		</title>
+
+	</head>
+	<BODY>
+		<p>Hello my name is Robbie</p>
+		<p>I love staying active, especially activities that I can bring my dogs along.</p>
+		<p>I also like working out and taking self defense classes</p>
+		<IMG SRC="images\aboutpic.jpg" alt="rottweiler"/>
+
+
+	</body>
+</HTML>
